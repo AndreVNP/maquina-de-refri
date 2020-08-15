2b4c7ce393317a4f6b2b7a322978d95b425f41bf
Andrevnp <andre.vnp@live.com>
2020-08-15 00:01:49 -0300

    Exibindo Produtos com *ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,8 @@ import { HttpClientModule } from '@angular/common/http'
 //Utils
 import { FormsModule } from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
+import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component'
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { MatInputModule } from '@angular/material/input'
     HomeComponent,
     PruductCrudComponent,
     ProductCreateComponent,
+    ProductReadComponent,
   ],
   imports: [
     BrowserModule,
